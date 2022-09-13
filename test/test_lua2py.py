ef36c6a7b073f8531512475782ad548ab25b0466
Manogna Choudary Potluri <manognacpotluri@gmail.com>
2022-09-13 02:12:22 -0400

    Adding test cases

--- test/test_lua2py.py
+++ test/test_lua2py.py
@@ -3,6 +3,7 @@ import sys
 from helpers import *
 from num import *
 from sym import *
+from data import *
 from test import test_lua2py
 import random
 
@@ -21,19 +22,55 @@ def test_sym():
 
 def test_num():
     num = Num()
-    for i in range(1,100):
+    for i in range(1,101):
         num.add(i)
-    mid, div = num.mid(), num.div()
+    mid, div = int(num.mid()), num.div()
     print(mid,div)
     return 50 <= mid and mid <= 52 and 30.5 < div and div < 32
 
 def test_bignum():
     num = Num()
     the['nums'] = 32
-    for i in range(1,1000):
+    for i in range(1,1001):
         num.add(i)
     oo(num.nums())
     return 32 == len(num._has)
+
+def test_csv():
+    global n
+    n = 0
+
+    def fun1(row):
+        global n
+        n = n + 1
+        if n > 10 :
+            return
+        else:
+            oo(row)
+
+    csv('./data/auto93.csv', fun1)
+    return True
+
+def test_data():
+    d = Data('./data/auto93.csv')
+    for col in d.cols.y:
+        oo(col)
+    return True
+
+def test_stats():
+    data = Data('./data/auto93.csv')
+    def div(col):
+        return col.div()
+    
+    def mid(col):
+        return col.mid()
+
+    print("xmid", o(data.stats(2, data.cols.x, mid)))
+    print("xdiv", o(data.stats(3, data.cols.x, div)))
+    print("ymid", o(data.stats(2, data.cols.y, mid)))
+    print("ydiv", o(data.stats(3, data.cols.y, div)))
+
+    return True
 
 def ALL():
     tests = dir(test_lua2py)
@@ -56,7 +93,7 @@ def run_tests(k):
     if k not in tests and k != "ALL":
         return
     
-    random.seed(0)
+    random.seed(10019)
 
     old = {}
     for u,v in the.items():
