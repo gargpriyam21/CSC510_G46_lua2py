17b1e2587c5acd246c4e81b346b4c84feab4a3e9
divyang02 <divyangdoshi02@gmail.com>
2022-09-20 00:07:08 -0400

    added requirements and fixed init

--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,3 @@
-import sys
+import sys, pathlib
 
-sys.path.insert(0, '/Users/priyamgarrg21/Documents/Priyam/NCSU/Curriculum/Fall22/CSC 510 - SE/Homeworks/CSC510_G46_lua2py/code')
+sys.path.insert(0, str(pathlib.Path().resolve())+'/code')
